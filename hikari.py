e25bea0dca959e35d9dba9aeeaed79ef8211cee4
Akmal Rizal <akmal.reezal@gmail.com>
2024-08-22 21:36:40 +0800

    Separate model inference code and add module selection prompt

--- hikari.py
+++ hikari.py
@@ -21,66 +21,113 @@ URI = "http://127.0.0.1:5000/v1/chat/completions"
 
 with open("character.json") as file:
     character = json.load(file)
-    verbose: bool = False
-    CHAR_NAME: str = character["name"]
-    GREETING: str = character["greeting"]
-    instruct_cmd: str = character["instruct_cmd"]
-    context: str = character["context"]
+    CHAR_NAME = character["name"]
+    INSTRUCT_CMD = character["instruct_cmd"]
+    MODULE_CMD = character["module_cmd"]
+    desc = character["description"]
+    module_ctx = character["module_context"]
+    main_ctx = character["main_context"]
 
-USER: str = "sorakee"
-# Nous-Hermes-SOLAR
-TEMPLATE: str = "ChatML"
-# Wizard-Vicuna
-# TEMPLATE: str = "Vicuna-v1.1"
-memory = [{"role": "assistant", "content": GREETING}] if GREETING != "" else []
-current_date = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+USER = "sorakee"
+TEMPLATE = "ChatML"
+VERBOSE = False
+short_mem = []
 
 
-# Process user's message
-async def process_message(sender_id, message_queue):
-    # Retrieve the first item in queue
-    user_item = message_queue[0]
-    user_msg: str = user_item["message"]
-    msg_date: datetime = user_item["datetime"]
-    msg_date_str: str = msg_date.strftime("%d/%m/%Y, %H:%M:%S")
-
-    context_with_date = "Today's date is " + current_date + ".\n\n" + context
-    memory.append({"role": "user", "content": user_msg})
+async def infer_model(context: str, command: str, tp: float=1.0) -> str:
+    """
+    Sends a prompt to the model through a POST request to a specified API endpoint.
+    
+    Parameters
+    ----------
+    context: str
+        The context of the prompt, which includes the AI's personality,
+        few-shot examples, instructions and any other additional information.
+    command: str
+        The instruction command, which will be followed by the AI.
+    tp: float
+        This parameter determines whether the output is more random and creative
+        or more predictable.
+    
+    Returns
+    -------
+    str
+        The output of the large language model
+    """
 
     headers = {
         "Content-Type": "application/json"
     }
 
     body = {
-        "messages": memory,
+        "messages": short_mem,
         "name1": USER,
         "mode": "chat-instruct",
         "character": CHAR_NAME,
-        "context": context_with_date,
-        "chat_instruct_command": instruct_cmd,
-        "instruction_template": TEMPLATE
+        "context": context,
+        "chat_instruct_command": command,
+        "instruction_template": TEMPLATE,
+        "temperature": tp
     }
 
     hikari_msg = ""
     
     async with aiohttp.ClientSession() as session:
-        response = await session.post(URI, json=body, headers=headers)
-    
-        if response.status == 200:
-            if verbose:
-                print(response.json(), "\n")
+        async with session.post(URI, json=body, headers=headers) as resp:
+            if resp.status == 200:
+                hikari_msg = await resp.json()
 
-            hikari_msg = await response.json()
-            hikari_msg = hikari_msg["choices"][0]["message"]["content"]
-            memory.append({"role": "assistant", "content": hikari_msg})
-        else:
-            hikari_msg = "ERROR: Please try again. Sorry!"
-    
-    hikari_msg = sent_tokenize(hikari_msg)
+                if VERBOSE:
+                    print(hikari_msg)
 
-    for msg in hikari_msg:
+                hikari_msg = hikari_msg["choices"][0]["message"]["content"]
+                short_mem.append({"role": "assistant", "content": hikari_msg})
+            else:
+                hikari_msg = "ERROR: Please try again. Sorry!"
+
+    return hikari_msg
+
+
+async def process_message(sender_id: int, message_queue: list):
+    """
+    Process user's message that is in the message queue
+    
+    Parameters
+    ----------
+    sender_id : int
+        The id of the sender
+    message_queue : list
+        The queue, consisting of user's message.
+    
+    Returns
+    -------
+    None
+
+    Description
+    -----------
+    This function adds current date and time to the AI's context.
+    It then sends a module selection prompt to the LLM, which returns
+    four possible outcomes - Conversation, Calendar, Image Generation, Weather.
+    Based on the outcome, this function will call a relevant function, which
+    returns a string to provide additional context to the main prompt.
+    """
+    
+    user_item = message_queue[0]
+    user_msg: str = user_item["message"]
+    msg_date: datetime = user_item["datetime"]
+    msg_date_str: str = msg_date.strftime("%d/%m/%Y, %H:%M:%S")
+
+    curr_date = f"Today's date is {datetime.now().strftime("%d/%m/%Y, %H:%M:%S")}"
+    curr_ctx = f"{curr_date}\n\n{desc}\n\n{module_ctx}"
+    short_mem.append({"role": "user", "content": user_msg})
+
+    # Splits generated result into a list of sentences
+    result = await infer_model(curr_ctx, MODULE_CMD)
+    result = sent_tokenize(result)
+
+    for msg in result:
         await bot.send_message(sender_id, msg)
-        await asyncio.sleep(1)
+        await asyncio.sleep(1.5)
     
     message_queue.pop(0)
 
--- test/intent-classify.py
+++ test/intent-classify.py
@@ -9,7 +9,9 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 classifier = TextClassificationPipeline(model=model, tokenizer=tokenizer)
 
-while True:
-    user = input("> ")
-    result = classifier(user)
-    print(result)
+
+if __name__ == "__main__":
+    while True:
+        user = input("> ")
+        result = classifier(user)
+        print(result)
