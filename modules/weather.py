6087b190b84cbef93443cda808b2112d75574928
Akmal Rizal <akmal.reezal@gmail.com>
2024-08-26 21:18:39 +0800

    Remove placeholder location and put it in dotenv file

--- modules/weather.py
+++ modules/weather.py
@@ -1,10 +1,16 @@
+import os
 import asyncio
 import python_weather
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LOCATION = os.getenv("LOCATION")
 
  
 async def get_weather():
     async with python_weather.Client() as client:
-        weather = await client.get("Pasir Gudang")
+        weather = await client.get(LOCATION)
         temp = f"{weather.temperature}°C"
         desc = weather.description
         print(desc)
